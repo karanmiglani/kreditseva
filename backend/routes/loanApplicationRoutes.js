afa6cfdb9755a1fe453a03f7e25d587c31018680
Karan Miglani <karanmiglani03@gmail.com>
2026-06-12 14:05:13 +0530

    Removed auth from save phone number api

--- backend/routes/loanApplicationRoutes.js
+++ backend/routes/loanApplicationRoutes.js
@@ -3,7 +3,7 @@ const { savePhoneNumber } = require('../controllers/loanApplicationController');
 const authMiddleware = require('../midllewares/authMiddleware');
 const router = express.Router();
 
-router.post('/save-phone-number',authMiddleware, savePhoneNumber)
+router.post('/save-phone-number', savePhoneNumber)
 
 
 module.exports = router;
